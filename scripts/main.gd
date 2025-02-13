913573cccbec6d4d44fd1339d91429df319cdce8
joshuafolkken <joshuafolkken@gmail.com>
2025-02-13 09:29:14 +0900

    run test #47

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,12 @@
+class_name Main
 extends Node
+
+var _test_color_rect: ColorRect
+
+
+func _init() -> void:
+	_test_color_rect = ColorRect.new()
+	add_child(_test_color_rect)
 
 
 # Called when the node enters the scene tree for the first time.
